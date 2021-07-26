fa532105e706657c9ad9c3866ee7123ee5733780
Riz <80712058+rizznn@users.noreply.github.com>
2021-07-26 00:28:24 -0400

    create the login route for authentication, compare the hashed password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,6 +5,7 @@ const { User } = require('../../models');
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
+      // excludes the password
       attributes: { exclude: ['password'] }
     })
       .then(dbUserData => res.json(dbUserData))
@@ -17,6 +18,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    // excludes the password
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
@@ -50,6 +52,31 @@ router.post('/', (req, res) => {
     });
 });
 
+// POST api/users/login
+// login route for authentication
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      // Verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });  
+});
+    
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -57,6 +84,7 @@ router.put('/:id', (req, res) => {
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
+      // adding hooks for hashing from User.js in models directory
       individualHooks: true,
       where: {
         id: req.params.id
