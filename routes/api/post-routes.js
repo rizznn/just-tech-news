adb8bfaf8da8548d103611c1b3beb35beb8dc512
Riz <80712058+rizznn@users.noreply.github.com>
2021-07-31 12:54:29 -0400

    model associations and create API routes for the Post Model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -8,16 +8,111 @@ const { Post, User } = require('../../models');
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-      // Query configuration
-      // configure the findAll method by customizing the attributes property
+        // Query configuration
+        // configure the findAll method by customizing the attributes property
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        // display the most recently added posts first.
+        order: [['created_at', 'DESC']], 
+        // JOIN to the user
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+       ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+// get-one query that will used a request parameter
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
       attributes: ['id', 'post_url', 'title', 'created_at'],
-      // JOIN to the user
       include: [
         {
-            model: User,
-            attributes: ['username']
+          model: User,
+          attributes: ['username']
         }
-        ]
+      ]
     })
-  
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});  
+
+// create a post
+router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
+
+// Because we'll be updating an existing entry, the idea is to first retrieve the post instance by id, then alter the value of the title on this instance of a post
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// to delete an entry
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+module.exports = router;
