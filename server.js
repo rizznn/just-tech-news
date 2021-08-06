4007b254050539bae359b266cee0e19f1ff0e3d0
Riz <80712058+rizznn@users.noreply.github.com>
2021-08-06 01:26:17 -0400

    create the homepage view - set up the template engine, create the homepage template, populate the template with sequelize data

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up Handlebars.js as the app's template engine of choice
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// til here
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
@@ -15,3 +25,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
