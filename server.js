5aea480fbeb251eddf2695cd58ab644f38ac50d1
Riz <80712058+rizznn@users.noreply.github.com>
2021-08-09 22:32:15 -0400

    write and fulfill test for plural words, date and URL formats, add helper functions to handlebars.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // set up Handlebars.js as the app's template engine of choice
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
+const hbs = exphbs.create({helpers});
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
